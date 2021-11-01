999a04f275fe0ad729e65c7a6f65a1b0703c77c8
Yongjin Kim <dydwls0669@gmail.com>
2021-11-01 16:52:33 +0900

    setup : import ormConfig로 변경

--- back/src/app.ts
+++ back/src/app.ts
@@ -3,10 +3,10 @@ import { createConnection } from 'typeorm';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import { Request, Response } from 'express';
-import { Routes } from './routes';
-import { User } from './entity/User';
+import { Routes } from './api/routes';
+import ormConfig from './config/ormConfig';
 
-createConnection()
+createConnection(ormConfig)
   .then(async (connection) => {
     const app = express();
     app.use(bodyParser.json());
@@ -24,21 +24,6 @@ createConnection()
 
     app.listen(8000);
 
-    await connection.manager.save(
-      connection.manager.create(User, {
-        firstName: 'Timber',
-        lastName: 'Saw',
-        age: 27,
-      }),
-    );
-    await connection.manager.save(
-      connection.manager.create(User, {
-        firstName: 'Phantom',
-        lastName: 'Assassin',
-        age: 24,
-      }),
-    );
-
-    console.log('Express server has started on port 3000. Open http://localhost:3000/users to see results');
+    console.log('Express server has started on port 8000. Open http://localhost:8000/users to see results');
   })
   .catch((error) => console.log(error));
