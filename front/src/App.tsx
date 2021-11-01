c79a2b1463b285cc75396a7f56de1c76f8b4eeb6
giljung456 <uhwan0723@gmail.com>
2021-11-01 14:58:01 +0900

    chore: 변경사항 App.tsx에 반영

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Main from './pages/Main';
-import Login from './pages/Login';
-import Make from './pages/Make';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle, Reset, theme } from './commons/style';
+import { ROUTE_MAIN, ROUTE_MAKE, ROUTE_LOGIN } from './commons/constants/route';
+import { Main, Make, Login } from './pages';
 
 function App(): JSX.Element {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" component={Main} exact />
-        <Route path="/login" component={Login} />
-        <Route path="/make" component={Make} />
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Reset />
+      <Router>
+        <Switch>
+          <Route path={ROUTE_MAIN} component={Main} exact />
+          <Route path={ROUTE_LOGIN} component={Login} />
+          <Route path={ROUTE_MAKE} component={Make} />
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 
