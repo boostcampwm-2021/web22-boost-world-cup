d5da7303cdeb6298ba4b15ca9d7aa0742cbc5af6
Yongjin Kim <dydwls0669@gmail.com>
2021-11-01 19:37:10 +0900

    chore : 코드 순서 변경

--- back/src/loaders/index.ts
+++ back/src/loaders/index.ts
@@ -2,8 +2,8 @@ import ormLoader from './orm';
 import expressLoader from './express';
 
 const indexLoader = async (app) => {
-  expressLoader(app);
   await ormLoader();
+  expressLoader(app);
 };
 
 export default indexLoader;
