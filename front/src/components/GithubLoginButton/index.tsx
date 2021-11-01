f7e231d46b8cd9b0c7c36cb7f4346e470d8b5d15
119wjw <119wjw@naver.com>
2021-11-01 22:54:46 +0900

    feat : github로그인 버튼 구현

---
+++ front/src/components/GithubLoginButton/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import { GoMarkGithub } from 'react-icons/go';
+
+function GithubLoginButton(): JSX.Element {
+  return (
+    <Container>
+      <GoMarkGithub />
+      <span>Continue with Github</span>
+    </Container>
+  );
+}
+
+const Container = styled('div')`
+  display: flex;
+  align-items: center;
+  padding-left: 20px;
+  font-size: 32px;
+  width: 390px;
+  height: 61px;
+  background-color: ${({ theme }) => theme.color.primary};
+  & span {
+    padding-left: 20px;
+    position: relative;
+    top: 2px;
+    font-size: 28px;
+  }
+`;
+
+export default GithubLoginButton;
