03ba6e2068479996e303df74218acdbf7cd00125
Yongjin Kim <dydwls0669@gmail.com>
2021-11-01 21:58:19 +0900

    setup : User entity 생성

--- back/src/entity/User.ts
+++ back/src/entity/User.ts
@@ -1,16 +1,22 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn } from 'typeorm';
 
 @Entity()
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  firstName: string;
+  @Column({ name: 'provider_id' })
+  providerId: string;
 
   @Column()
-  lastName: string;
+  nickname: string;
+
+  @Column()
+  gender: number;
 
   @Column()
   age: number;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
 }
