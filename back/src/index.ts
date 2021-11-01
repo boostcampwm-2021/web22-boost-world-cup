7341dc5b4b4f94ad9d56c459739eace12cd85fb4
Yongjin Kim <dydwls0669@gmail.com>
2021-11-01 18:29:36 +0900

    chore : index.ts로 이름 변경
    app.ts => index.ts 로 파일 이름 변경

---
+++ back/src/index.ts
@@ -0,0 +1,14 @@
+import 'dotenv/config';
+import loader from './loaders';
+import * as express from 'express';
+
+const startServer = async () => {
+  const app = express();
+  await loader(app);
+  const port = process.env.PORT || 8000;
+  app.listen(port, () => {
+    console.log(`Express server has started on ${port}. Open http://localhost:${port}`);
+  });
+};
+
+startServer();
