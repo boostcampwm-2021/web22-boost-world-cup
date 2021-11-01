79b4d890b1177ed7cdf0085f7e46bf2da7797517
giljung456 <uhwan0723@gmail.com>
2021-11-01 14:57:23 +0900

    refactor: route 상수처리

---
+++ front/src/commons/constants/route.ts
@@ -0,0 +1,3 @@
+export const ROUTE_MAIN = '/main';
+export const ROUTE_LOGIN = '/login';
+export const ROUTE_MAKE = '/make';
---
+++ front/src/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as Login } from './Login';
+export { default as Main } from './Main';
+export { default as Make } from './Make';
