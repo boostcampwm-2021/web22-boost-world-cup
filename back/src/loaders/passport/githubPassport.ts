5ed55bcf795948ba4c211d7b584a8096c5d99881
Yongjin Kim <dydwls0669@gmail.com>
2021-11-02 23:19:34 +0900

    feat : passport 이용한 github로그인

--- back/src/loaders/passport/githubPassport.ts
+++ back/src/loaders/passport/githubPassport.ts
@@ -4,13 +4,13 @@ import * as passportGithub from 'passport-github';
 import githubStrategyConfig from '../../config/githubStrategy';
 const GitHubStrategy = passportGithub.Strategy;
 
-import { findByProviderId, save } from '../../services/authService';
+import { findByProviderId, saveInitUser } from '../../services/authService';
 
 const githubPassportInit = () => {
   passport.use(
     new GitHubStrategy(githubStrategyConfig, async (accessToken, refreshToken, profile, done) => {
       const providerId = profile.id;
-      const user = (await findByProviderId(providerId)) || (await save(providerId));
+      const user = (await findByProviderId(providerId)) || (await saveInitUser(providerId));
       done(null, user);
     }),
   );
