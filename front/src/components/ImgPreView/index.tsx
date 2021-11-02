f44d8f077684dd983f6dfa87f82361825b20e27c
giljung456 <uhwan0723@gmail.com>
2021-11-02 23:54:04 +0900

    feat: ImgPreView 컴포넌트 추가 구현

--- front/src/components/ImgPreView/index.tsx
+++ front/src/components/ImgPreView/index.tsx
@@ -1,11 +1,34 @@
 import React from 'react';
+import { MdCancel } from 'react-icons/md';
+import styled from 'styled-components';
 
 interface Props {
+  onDelete: React.MouseEventHandler<HTMLButtonElement>;
   src: string;
 }
 
-function ImgPreView({ src }: Props): JSX.Element {
-  return <img src={src} width="143px" height="160px" style={{ borderRadius: '20px' }} alt="" />;
+function ImgPreView({ onDelete, src }: Props): JSX.Element {
+  return (
+    <Container>
+      <Btn type="button" onClick={onDelete}>
+        <MdCancel size={40} color="red" />
+      </Btn>
+      <img src={src} width="143px" height="160px" style={{ borderRadius: '20px' }} alt="" />
+    </Container>
+  );
 }
+
+const Container = styled.li`
+  position: relative;
+`;
+
+const Btn = styled.button`
+  position: absolute;
+  background-color: ${({ theme }) => theme.color.white};
+  border-radius: 50%;
+  height: 40px;
+  right: -20px;
+  top: -20px;
+`;
 
 export default ImgPreView;
