0938f27b5f9a872b16af184aedb6fb01b01989de
yujin <yujin1760@naver.com>
2021-11-02 18:41:41 +0900

    design: 태그 목록 carousel 애니메이션 적용

--- front/src/components/TagList/index.tsx
+++ front/src/components/TagList/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
+import Slider, { Settings } from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const tagList: Array<string> = [
   '전체',
   '아이돌',
-  '음식',
+  '제일 좋아하는',
   '영화',
   '인기있는',
   '연예인',
@@ -14,33 +17,64 @@ const tagList: Array<string> = [
   '맛없는',
   '유행하는',
   '핫한',
+  '음식',
 ];
 function TagList() {
+  const settings = {
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    variableWidth: true,
+    draggable: false,
+    speed: 300,
+  };
   return (
-    <TagContainer>
+    <TagContainer {...settings}>
       {tagList.map((tag) => (
-        <Tag>{tag}</Tag>
+        <div>
+          <h3>{tag}</h3>
+        </div>
       ))}
     </TagContainer>
   );
 }
 
-const TagContainer = styled.section`
+const TagContainer = styled(Slider)`
   display: flex;
   justify-content: center;
   align-items: center;
-`;
-const Tag = styled.section`
-  background-color: ${({ theme }) => theme.color.primary};
-  padding: 10px 20px;
-  margin: 10px;
-  white-space: noWrap;
-  border-radius: 8px;
-  cursor: pointer;
-  &:hover {
-    background-color: ${({ theme }) => theme.color.pink};
-    color: ${({ theme }) => theme.color.highlight};
+  text-align: center;
+  margin: auto;
+  margin-top: 20px;
+  width: 90vw;
+  .slick-slide {
+    margin: 0 14px;
+    background-color: ${({ theme }) => theme.color.primary};
+    white-space: noWrap;
+    padding: 12px;
+    border-radius: 12px;
+    cursor: pointer;
+    transition: all 300ms ease-in;
+    &:hover {
+      background-color: ${({ theme }) => theme.color.pink};
+      color: ${({ theme }) => theme.color.highlight};
+      transform: scale(1.1);
+    }
+  }
+  .slick-prev:before {
+    color: ${({ theme }) => theme.color.pink};
+  }
+  .slick-next:before {
+    color: ${({ theme }) => theme.color.pink};
+  }
+  .slick-prev {
+    left: 3% 
+    z-index: 1;
+  }
+  .slick-next {
+    right: 3% 
+    z-index: 1;
   }
 `;
-
 export default TagList;
--- front/src/pages/Main/index.tsx
+++ front/src/pages/Main/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import styled from 'styled-components';
 import CardList from '../../components/WorldcupList';
 import Header from '../../components/Header';
 import TagList from '../../components/TagList';
@@ -7,12 +8,15 @@ function Main(): JSX.Element {
   const [isLogin, setIsLogin] = useState(true);
   const canSearch = true;
   return (
-    <>
+    <Wrapper>
       <Header isLogin={isLogin} canSearch={canSearch} />
       <TagList />
       <CardList />
-    </>
+    </Wrapper>
   );
 }
+const Wrapper = styled.body`
+  background-color: #fdf8f7;
+`;
 
 export default Main;
