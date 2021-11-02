a776d17ceaf2273e153ad1e516311d4ada87577e
Yongjin Kim <dydwls0669@gmail.com>
2021-11-02 14:24:49 +0900

    feat : cookieParser 추가

--- back/src/loaders/express.ts
+++ back/src/loaders/express.ts
@@ -1,10 +1,12 @@
 import { Request, Response } from 'express';
 import router from '../api';
 import * as express from 'express';
+import * as cookieParser from 'cookie-parser';
 
 const expressLoader = (app) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
 
   router.forEach((route) => {
     (app as any)[route.method](route.route, (req: Request, res: Response, next: Function) => {
