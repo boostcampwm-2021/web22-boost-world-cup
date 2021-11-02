39b1dd346fd36b36d4dd16812e322cd91c14ef1e
Yongjin Kim <dydwls0669@gmail.com>
2021-11-02 19:32:11 +0900

    config : github Oauth 설정 추가

---
+++ back/src/config/githubStrategy.ts
@@ -0,0 +1,9 @@
+import 'dotenv/config';
+
+const githubStrategyConfig = {
+  clientID: process.env.GITHUB_CLIENT_ID,
+  clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  callbackURL: process.env.GITHUB_CALLBACK_URL,
+};
+
+export default githubStrategyConfig;
