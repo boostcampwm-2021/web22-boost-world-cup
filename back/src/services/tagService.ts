59241b79ee57541211d21755f38c07f3c1d83602
119wjw <119wjw@naver.com>
2021-11-03 18:58:39 +0900

    refactor : tagService 로직 수정 - 쿼리에서 데이터를 미리 선별

--- back/src/services/tagService.ts
+++ back/src/services/tagService.ts
@@ -3,11 +3,17 @@ import { getRepository } from 'typeorm';
 
 export const findTen = async () => {
   const tagRepository = getRepository(Keyword);
-  const tags = await tagRepository.find();
-	tags.sort((a,b) => b.cnt - a.cnt);
+  const tags = await tagRepository.find({
+		select : ["name"],
+		order : {
+			cnt : "DESC"
+		},
+		take : 10,
+	});
 	return {
 		result : "success",
 		message : null,
-		data : {tags : tags.map(tag => tag.name).slice(0,10)},
+		data : { tags : tags.map(tag => tag.name) },
 	}
 };
+
