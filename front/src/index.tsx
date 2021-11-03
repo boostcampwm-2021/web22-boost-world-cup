5ee5f252dd75d79d837b3f9f27c8e77eaf853b9a
Yongjin Kim <dydwls0669@gmail.com>
2021-11-04 01:21:45 +0900

    setup : recoilRoot 추가

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <React.Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </React.Suspense>
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById('root'),
 );
