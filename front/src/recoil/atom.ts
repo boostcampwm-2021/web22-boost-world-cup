98269f9607d1795a553fb476e78abb7f1b1e6fdb
Yongjin Kim <dydwls0669@gmail.com>
2021-11-04 13:33:59 +0900

    feat : user Login atom 추가

--- front/src/recoil/atom.ts
+++ front/src/recoil/atom.ts
@@ -1,5 +1,10 @@
 import { atom } from 'recoil';
 
+export const loginState = atom({
+  key: 'loginState',
+  default: false,
+});
+
 export const userState = atom({
   key: 'userState',
   default: {},
