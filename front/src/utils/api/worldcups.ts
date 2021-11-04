9ee21b8795eeaf81971687d64c77aa0e77324d73
yujin <yujin1760@naver.com>
2021-11-04 17:14:07 +0900

    feat: tag filtering front api 추가

--- front/src/utils/api/worldcups.ts
+++ front/src/utils/api/worldcups.ts
@@ -4,11 +4,22 @@ interface pagingQueryType {
   offset: number;
   limit: number;
 }
+interface searchQuerytype {
+  keyword: string;
+}
 export const getWorldcupList = async (query: pagingQueryType) => {
   const response = await axios.get('/api/worldcups', {
     params: {
       offset: query.offset,
       limit: query.limit,
+    },
+  });
+  return response.data.data.worldcup;
+};
+export const getWorldcupListByKeyword = async (query: searchQuerytype) => {
+  const response = await axios.get('/api/worldcups', {
+    params: {
+      keyword: query.keyword,
     },
   });
   return response.data.data.worldcup;
