1447732824364d441e3497bfa86e48f21d6ee906
Yongjin Kim <dydwls0669@gmail.com>
2021-11-04 13:34:43 +0900

    feat : 로그인되어 있을 때 홈으로 redirect

--- front/src/pages/Login/index.tsx
+++ front/src/pages/Login/index.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Redirect } from 'react-router-dom';
 import styled from 'styled-components';
 import { GoMarkGithub } from 'react-icons/go';
 import { SiKakaotalk } from 'react-icons/si';
 import { FcGoogle } from 'react-icons/fc';
+import { getUser } from '../../utils/api/auth';
 import SocialLoginButton from '../../components/SocialLoginButton';
 import logo from '../../images/logo.png';
 
@@ -10,7 +12,22 @@ const githubOauthUrl = `https://github.com/login/oauth/authorize?client_id=${pro
 const kakaoOauthUrl = `https://kauth.kakao.com/oauth/authorize?client_id=${process.env.REACT_APP_OAUTH_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_OAUTH_CLIENT_ID}&response_type=code`;
 
 const Login = (): JSX.Element => {
-  return (
+  const [isLogin, setIsLogin] = useState(false);
+
+  const getUserInfo = async () => {
+    const user = await getUser();
+    if (Object.keys(user).length !== 0) {
+      setIsLogin(true);
+    }
+  };
+
+  useEffect(() => {
+    getUserInfo();
+  }, []);
+
+  return isLogin ? (
+    <Redirect to="/" />
+  ) : (
     <Container>
       <img src={logo} alt="logo" width="220px" height="220px" />
       <Title>Welcome to world cup</Title>
