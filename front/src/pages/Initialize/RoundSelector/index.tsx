9fd7dea872a6ff8f7f5cf476e5e1b6bedb2079e2
Yongjin Kim <dydwls0669@gmail.com>
2021-11-08 18:05:59 +0900

    design : css 보완

--- front/src/pages/Initialize/RoundSelector/index.tsx
+++ front/src/pages/Initialize/RoundSelector/index.tsx
@@ -54,7 +54,7 @@ const BackWindow = styled.div<{ isOpen: boolean }>`
 
 const RoundInput = styled.div`
   display: flex;
-  width: 80%;
+  width: 100%;
   height: 49px;
   background-color: ${({ theme }) => theme.color.white};
   ${({ theme }) => theme.fontStyle.bodyBold}
--- front/src/pages/Initialize/index.tsx
+++ front/src/pages/Initialize/index.tsx
@@ -15,17 +15,68 @@ function Initialize(): JSX.Element {
     <>
       <Header type="header" isLogin />
       <Container>
-        <img src={logo} alt="logo" width="220px" height="220px" />
-        <Title>여자 배우 이상형 월드컵</Title>
-        <Desc>정면 위주의 레전드 사진 (객관적인 얼굴 판단 가능)</Desc>
-        <Temp1>총 라운드를 선택하세요.</Temp1>
-        <RoundSelector round={round} roundSelector={roundSelector} />
-        <Temp1>총 221명의 후보 중 무작위 32명이 대결합니다.</Temp1>
+        <ContainerInfo>
+          <img src={logo} alt="logo" width="220px" height="200px" />
+          <Title>여자 배우 이상형 월드컵여자 배우</Title>
+          <Desc>정면 위주의 레전드 사진 (객관적인 얼굴 판단 가능)</Desc>
+        </ContainerInfo>
+        <ContainerRound>
+          <SubContainerRound>
+            <span>총 라운드를 선택하세요.</span>
+            <RoundSelector round={round} roundSelector={roundSelector} />
+            <span>총 221명의 후보 중 무작위 32명이 대결합니다.</span>
+          </SubContainerRound>
+        </ContainerRound>
       </Container>
       <BackWindow />
     </>
   );
 }
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 1000px;
+  height: 811px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 5%);
+  background-color: ${({ theme }) => theme.color.white};
+  z-index: 3;
+`;
+
+const ContainerInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-left: 3%;
+  padding-right: 3%;
+`;
+
+const ContainerRound = styled.div`
+  background-color: ${({ theme }) => theme.color.primary};
+  z-index: 3;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 1%;
+`;
+
+const SubContainerRound = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 3%;
+  padding-bottom: 3%;
+  width: 80%;
+  span {
+    align-self: baseline;
+    margin-top: 1%;
+    margin-bottom: 1%;
+    ${({ theme }) => theme.fontStyle.bodyBold}
+  }
+`;
 
 const BackWindow = styled.div`
   position: fixed;
@@ -38,30 +89,14 @@ const BackWindow = styled.div`
   left: 0;
 `;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  width: 1000px;
-  height: 811px;
-  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
-  position: relative;
-  left: 50%;
-  transform: translate(-50%, 5%);
-  background-color: ${({ theme }) => theme.color.primary};
-  z-index: 3;
-`;
-
 const Title = styled.div`
+  text-align: center;
   ${({ theme }) => theme.fontStyle.h1Bold}
 `;
 
 const Desc = styled.div`
+  text-align: center;
   ${({ theme }) => theme.fontStyle.h3}
-`;
-
-const Temp1 = styled.div`
-  ${({ theme }) => theme.fontStyle.bodyBold}
 `;
 
 export default Initialize;
