f1094b939cb7ea963b617cedb885d921794fc1ac
giljung456 <uhwan0723@gmail.com>
2021-11-09 21:17:03 +0900

    feat: make페이지에서 탭을 변경할 때마다 모든 이미지를 한번에 불러오던 것을 pagination을 이용하여 lazy loading되도록

--- front/src/pages/Make/index.tsx
+++ front/src/pages/Make/index.tsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useState } from 'react';
+import React, { useReducer, useState, useEffect } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 import { Header, MakeWorldcupForm, ImgTable, MakePageTabBar } from '../../components';
@@ -7,6 +7,8 @@ import worldcupFormReducer, { initialWorldcupFormState } from './reducer';
 
 function Make(): JSX.Element {
   const [currentTab, setCurrentTab] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [preViews, setPreViews] = useState<ImgInfo[]>([]);
   const [worldcupFormState, worldcupFormDispatcher] = useReducer(worldcupFormReducer, initialWorldcupFormState);
   const { title, desc, imgInfos } = worldcupFormState;
 
@@ -37,6 +39,7 @@ function Make(): JSX.Element {
       return { name: file.name, key };
     });
     worldcupFormDispatcher({ type: 'CHANGE_IMG_INFOS', payload: [...imgInfos, ...newImgInfos] });
+    setPreViews((prevPreViews) => [...prevPreViews, ...newImgInfos]);
   };
 
   const onDeleteImg = (imgKey: string) => {
@@ -71,7 +74,7 @@ function Make(): JSX.Element {
   };
 
   const getOnTabChange = (tabNum: number) => {
-    return (e: React.MouseEvent<HTMLButtonElement>) => {
+    return () => {
       if (currentTab === tabNum) return;
       setCurrentTab(tabNum);
     };
@@ -100,6 +103,10 @@ function Make(): JSX.Element {
     };
   };
 
+  useEffect(() => {
+    setPreViews([]);
+  }, [currentTab]);
+
   return (
     <>
       <Header type="header" isLogin />
@@ -112,7 +119,7 @@ function Make(): JSX.Element {
             onAddImgs={onAddImgs}
             onDeleteImg={onDeleteImg}
             onStore={onStore}
-            imgInfos={imgInfos}
+            imgInfos={preViews}
           />
         )}
         {currentTab === 2 && (
@@ -122,6 +129,8 @@ function Make(): JSX.Element {
             getOnImgNameChange={getOnImgNameChange}
             getOnImgChange={getOnImgChange}
             onStore={onStore}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
           />
         )}
       </Content>
