c32bdd1027331694e70a66ecaae3f1890121b664
yujin <yujin1760@naver.com>
2021-11-10 15:26:56 +0900

    refactor: ===0 => ! 으로 바꾸기

--- front/src/components/WorldcupList/index.tsx
+++ front/src/components/WorldcupList/index.tsx
@@ -34,12 +34,12 @@ function WorldcupList({ offset, setOffset, selectedTag, searchWord }: Props): JS
     if (searchWord) newItems = await getWorldcupListBySearch({ offset, limit, search: searchWord });
     else if (selectedTag) newItems = await getWorldcupListByKeyword({ offset, limit, keyword: selectedTag });
     else newItems = await getWorldcupList({ offset, limit });
-    if (newItems.length === 0 && observer.current) {
+    if (!newItems.length && observer.current) {
       observer.current.disconnect();
       setLoading(false);
       return;
     }
-    if (offset === 0) setItems([...newItems]);
+    if (!offset) setItems([...newItems]);
     else setItems([...items, ...newItems]);
   };
   useEffect(() => {
