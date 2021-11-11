e34173b443cf94ec447f0bab53c2fe7806fc76a9
giljung456 <uhwan0723@gmail.com>
2021-11-11 14:31:11 +0900

    refactor: worldcupForm 에서 쓰던 action타입을 더 일반화 하여 모든 form에서 사용할 수 있게 함

--- front/src/pages/Make/reducer.ts
+++ front/src/pages/Make/reducer.ts
@@ -1,29 +1,14 @@
 import { WorldcupFormState } from '../../types/States';
-import { WorldcupFormAction } from '../../types/Actions';
+import { FormAction } from '../../types/Actions';
 
 export const initialWorldcupFormState: WorldcupFormState = {
   title: '',
   desc: '',
+  keywords: [],
   imgInfos: [],
 };
 
-export default (state: WorldcupFormState, action: WorldcupFormAction): WorldcupFormState => {
+export default (state: WorldcupFormState, action: FormAction<WorldcupFormState>): WorldcupFormState => {
   const { type, payload } = action;
-  switch (type) {
-    case 'CHANGE_TITLE': {
-      if (typeof payload !== 'string') throw new Error(`payload of Action: CHANGE_TITLE must be string`);
-      return { ...state, title: payload };
-    }
-    case 'CHANGE_DESC': {
-      if (typeof payload !== 'string') throw new Error(`payload of Action: CHANGE_DESC must be string`);
-      return { ...state, desc: payload };
-    }
-    case 'CHANGE_IMG_INFOS': {
-      if (typeof payload === 'string') throw new Error(`payload of Action: CHANGE_IMG_INFOS must be ImgInfo`);
-      return { ...state, imgInfos: payload };
-    }
-    default: {
-      throw new Error(`Unexpected Action type: ${type}`);
-    }
-  }
+  return { ...state, [type]: payload };
 };
