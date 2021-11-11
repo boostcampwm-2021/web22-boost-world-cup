46b744d862cf7b5316388d3eb1161d1b03357a20
Yongjin Kim <dydwls0669@gmail.com>
2021-11-11 14:27:53 +0900

    chore : 필요없는 코드 삭제

--- back/src/api/gameRouter.ts
+++ back/src/api/gameRouter.ts
@@ -4,6 +4,5 @@ import controller from '../controller/gameController';
 const router = Router();
 
 router.get('/candidates', controller.getCandidates);
-router.post('/result', controller.result);
 
 export default router;
