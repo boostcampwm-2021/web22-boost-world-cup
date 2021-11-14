5d71c51afe398173d300f73d5f3d6e18883fbd92
giljung456 <uhwan0723@gmail.com>
2021-11-14 17:23:15 +0900

    feat: make 페이지의 상태 중 willUploadFiles와 presignedURLs를 리듀서로 분리 #68

--- front/src/pages/Make/reducer.ts
+++ front/src/pages/Make/reducer.ts
@@ -24,7 +24,7 @@ export const initialWorldcupFormState: WorldcupFormState = {
   imgInfos: [],
 };
 
-export default (state: WorldcupFormState, action: WorldcupFormAction): WorldcupFormState => {
+export const worldcupFormReducer = (state: WorldcupFormState, action: WorldcupFormAction): WorldcupFormState => {
   switch (action.type) {
     case 'CHANGE_TITLE': {
       const { payload: newTitle } = action;
@@ -94,5 +94,38 @@ export default (state: WorldcupFormState, action: WorldcupFormAction): WorldcupF
     default: {
       throw new Error('Unexpected action type');
     }
+  }
+};
+
+export interface UploadState {
+  willUploadFiles: File[];
+  presignedURLs: string[];
+}
+
+export type UploadAction =
+  | { type: 'ADD_FILES'; payload: File[] }
+  | { type: 'ADD_PRESIGNED_URL'; payload: string[] }
+  | { type: 'RESET' };
+
+export const initialUploadState: UploadState = {
+  willUploadFiles: [],
+  presignedURLs: [],
+};
+
+export const uploadReducer = (state: UploadState, action: UploadAction): UploadState => {
+  switch (action.type) {
+    case 'ADD_FILES': {
+      const { payload: newFiles } = action;
+      return { ...state, willUploadFiles: [...state.willUploadFiles, ...newFiles] };
+    }
+    case 'ADD_PRESIGNED_URL': {
+      const { payload: newPresignedURLs } = action;
+      return { ...state, presignedURLs: [...state.presignedURLs, ...newPresignedURLs] };
+    }
+    case 'RESET': {
+      return { willUploadFiles: [], presignedURLs: [] };
+    }
+    default:
+      throw new Error('Unexpected action type');
   }
 };
