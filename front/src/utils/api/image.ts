0545513840724b6ba52ac81bceeaf801e2514f75
giljung456 <uhwan0723@gmail.com>
2021-11-14 17:18:35 +0900

    refactor: useApiRequest를 사용하기 위해, 이미지 업로드하는 api 호출로직 util로 분리 #68

--- front/src/utils/api/image.ts
+++ front/src/utils/api/image.ts
@@ -2,3 +2,8 @@ import axios, { AxiosResponse } from 'axios';
 
 export const getSignedURLs = (contentTypes: string[]): Promise<AxiosResponse> =>
   axios.post('/api/bucket/url', { contentTypes });
+
+export const uploadImage = (presignedURL: string, file: ArrayBuffer, type: string): Promise<AxiosResponse> =>
+  axios.put(presignedURL, file, {
+    headers: { 'Content-Type': type, 'x-amz-acl': 'public-read' },
+  });
