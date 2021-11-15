61389db2da2231ccbf925a839fbd00a24742d716
Yongjin Kim <dydwls0669@gmail.com>
2021-11-15 23:54:24 +0900

    fix: 댓글 조회 api 수정

--- back/src/services/commentService.ts
+++ back/src/services/commentService.ts
@@ -6,6 +6,13 @@ import { Worldcup } from '../entity/Worldcup';
 export const findByWorldcupId = async (worldcupId: string) => {
   return await getRepository(Comment)
     .createQueryBuilder('comment')
+    .select([
+      'comment.id AS id',
+      'comment.message AS message',
+      'comment.created_at AS createdAt',
+      'user.nickname AS nickname',
+    ])
+    .innerJoin('comment.user', 'user')
     .where('comment.worldcup_id= :id', { id: worldcupId })
     .execute();
 };
