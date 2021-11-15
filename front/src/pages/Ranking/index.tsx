2340741198ff60041a07a86de8224490caefdbbc
yujin <yujin1760@naver.com>
2021-11-16 02:20:48 +0900

    design: 전체 통계 리스트 ui

--- front/src/pages/Ranking/index.tsx
+++ front/src/pages/Ranking/index.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import { Header, MakeWorldcupForm, ImgTable, MakePageTabBar } from '../../components';
+import { Header, RankingList } from '../../components';
 
 function Ranking(): JSX.Element {
   return (
     <>
       <Header type="header" />
+      <RankingContent>
+        <RankingList />
+      </RankingContent>
     </>
   );
 }
 
+const RankingContent = styled.div`
+  width: 90vw;
+  margin: 0 auto;
+  margin-top: 5em;
+  padding: 2em 6em 2em 0;
+  background-color: skyblue;
+`;
 export default Ranking;
