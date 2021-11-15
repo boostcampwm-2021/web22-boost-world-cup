2664d6315f650cad09806d247e08fcbf2c098f18
Yongjin Kim <dydwls0669@gmail.com>
2021-11-15 16:44:51 +0900

    refactor: infoService로 로직분리
    related : #74

--- back/src/controller/rankingController.ts
+++ back/src/controller/rankingController.ts
@@ -1,18 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
-import { Info } from '../entity/Info';
 import * as candidateService from '../services/candidateService';
-
-export enum GENDER {
-  MALE = 1,
-  FEMALE,
-}
-export enum AGE {
-  TEENS = 1,
-  TWENTIES,
-  THIRTIES,
-  FORTIES,
-  ETC,
-}
+import { makeInfoData, setInfoData } from '../services/infoService';
 
 const rankingController = {
   saveCurrentResult: async (request: Request, response: Response, next: NextFunction) => {
@@ -28,68 +16,9 @@ const rankingController = {
     loseCandidate.showCnt += 1;
 
     if (!winCandidate.info) {
-      winCandidate.info = new Info();
-      switch (gender) {
-        case GENDER.MALE:
-          winCandidate.info.male = 1;
-          break;
-        case GENDER.FEMALE:
-          winCandidate.info.female = 1;
-          break;
-        default:
-          break;
-      }
-      switch (age) {
-        case AGE.TEENS:
-          winCandidate.info.teens = 1;
-          break;
-        case 2:
-          winCandidate.info.twenties = 1;
-          break;
-        case 3:
-          winCandidate.info.thirties = 1;
-          break;
-        case 4:
-          winCandidate.info.forties = 1;
-          break;
-        case 5:
-          winCandidate.info.etc = 1;
-          break;
-        default:
-          break;
-      }
-      winCandidate.info.total = 1;
+      winCandidate.info = makeInfoData(gender, age);
     } else {
-      switch (age) {
-        case 1:
-          winCandidate.info.teens += 1;
-          break;
-        case 2:
-          winCandidate.info.twenties += 1;
-          break;
-        case 3:
-          winCandidate.info.thirties += 1;
-          break;
-        case 4:
-          winCandidate.info.forties += 1;
-          break;
-        case 5:
-          winCandidate.info.etc += 1;
-          break;
-        default:
-          break;
-      }
-      switch (gender) {
-        case 1:
-          winCandidate.info.male += 1;
-          break;
-        case 2:
-          winCandidate.info.female += 1;
-          break;
-        default:
-          break;
-      }
-      winCandidate.info.total += 1;
+      winCandidate.info = setInfoData(winCandidate.info, gender, age);
     }
     await candidateService.saveCandidate(winCandidate);
     await candidateService.saveCandidate(loseCandidate);
--- back/src/services/infoService.ts
+++ back/src/services/infoService.ts
@@ -1,10 +1,73 @@
 import { Info } from '../entity/Info';
 import { getRepository } from 'typeorm';
 import { Candidate } from '../entity/Candidate';
+import { GENDER, AGE } from '../types/common';
 
-export const makeInfoData = async (candidate: Candidate) => {
-  const infoRepository = getRepository(Info);
+export const makeInfoData = (gender: number, age: number): Info => {
   const info = new Info();
-  info.candidate = candidate;
-  await infoRepository.save(info);
+  switch (gender) {
+    case GENDER.MALE:
+      info.male = 1;
+      break;
+    case GENDER.FEMALE:
+      info.female = 1;
+      break;
+    default:
+      break;
+  }
+  switch (age) {
+    case AGE.TEENS:
+      info.teens = 1;
+      break;
+    case 2:
+      info.twenties = 1;
+      break;
+    case 3:
+      info.thirties = 1;
+      break;
+    case 4:
+      info.forties = 1;
+      break;
+    case 5:
+      info.etc = 1;
+      break;
+    default:
+      break;
+  }
+  info.total = 1;
+  return info;
+};
+
+export const setInfoData = (info: Info, gender: number, age: number): Info => {
+  switch (gender) {
+    case GENDER.MALE:
+      info.male += 1;
+      break;
+    case GENDER.FEMALE:
+      info.female += 1;
+      break;
+    default:
+      break;
+  }
+  switch (age) {
+    case AGE.TEENS:
+      info.teens += 1;
+      break;
+    case 2:
+      info.twenties += 1;
+      break;
+    case 3:
+      info.thirties += 1;
+      break;
+    case 4:
+      info.forties += 1;
+      break;
+    case 5:
+      info.etc += 1;
+      break;
+    default:
+      break;
+  }
+  info.total += 1;
+  return info;
 };
