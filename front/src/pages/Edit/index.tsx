9d30716644747522e9290a8538ec156d2b8670b1
giljung456 <uhwan0723@gmail.com>
2021-11-17 01:05:57 +0900

    feat: Edit에서 MakeWorldcupForm으로 onDescBlur를 내려주는 부분 추가 #89
    onDescBlur 내부에서는 desc 인풋이 포커스를 잃을 때마다 이전 값과 비교한 뒤, 다르면 patch 요청을 보냅니다.

--- front/src/pages/Edit/index.tsx
+++ front/src/pages/Edit/index.tsx
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 import { Header, MakeWorldcupForm, ImgTable, MakePageTabBar } from '../../components';
 import useApiRequest, { NULL, REQUEST, SUCCESS, FAILURE } from '../../hooks/useApiRequest';
 import { useTabBar, useWorldcupForm, useImgInfos, usePagination } from '../../hooks';
-import { getWorldcupMetadata, getWorldcupCandidates, patchWorldcupTitle } from '../../utils/api/worldcups';
+import {
+  getWorldcupMetadata,
+  getWorldcupCandidates,
+  patchWorldcupTitle,
+  patchWorldcupDesc,
+} from '../../utils/api/worldcups';
 import { ImgInfo } from '../../types/Datas';
 
 function Edit(): JSX.Element {
@@ -17,6 +22,7 @@ function Edit(): JSX.Element {
   const [getMetadataResult, getMetadataDispatcher] = useApiRequest(getWorldcupMetadata);
   const [getCandidatesResult, getCandidatesDispatcher] = useApiRequest(getWorldcupCandidates);
   const [patchTitleResult, patchTitleDispatcher] = useApiRequest(patchWorldcupTitle);
+  const [patchDescResult, patchDescDispatcher] = useApiRequest(patchWorldcupDesc);
   const worldcupId = useMemo(() => window.location.pathname.split('/')[2], [window.location]);
 
   const getSignedURLsSuccessEffect = (addedImgs: ImgInfo[]) => {
@@ -27,6 +33,12 @@ function Edit(): JSX.Element {
     if (title === target.value) return;
     patchTitleDispatcher({ type: REQUEST, requestProps: [worldcupId, target.value] });
     worldcupFormDispatcher({ type: 'CHANGE_TITLE', payload: target.value });
+  };
+  const onDescBlur: React.FocusEventHandler<HTMLInputElement> = ({ target }) => {
+    const { desc } = worldcupFormState;
+    if (desc === target.value) return;
+    patchDescDispatcher({ type: REQUEST, requestProps: [worldcupId, target.value] });
+    worldcupFormDispatcher({ type: 'CHANGE_DESC', payload: target.value });
   };
 
   useEffect(() => {
@@ -86,6 +98,7 @@ function Edit(): JSX.Element {
             imgInfos={addedImgs}
             worldcupFormState={worldcupFormState}
             onTitleBlur={onTitleBlur}
+            onDescBlur={onDescBlur}
             onKeywordsChange={({ target }) => {
               worldcupFormDispatcher({ type: 'ADD_KEYWORD', payload: target.value });
             }}
