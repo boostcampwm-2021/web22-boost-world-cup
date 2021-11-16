80635c02545bc1f0571d5db9657874de7cacafa2
giljung456 <uhwan0723@gmail.com>
2021-11-16 18:41:07 +0900

    feat: edit 페이지에서 사용할 candidate를 생성하는 api를 호출하는 함수 추가 #89

--- front/src/utils/api/candidate.ts
+++ front/src/utils/api/candidate.ts
@@ -1,3 +1,7 @@
 import axios, { AxiosResponse } from 'axios';
+import { ImgInfo } from '../../types/Datas';
 
 export const deleteCandidate = (key: string): Promise<AxiosResponse> => axios.delete(`/api/candidates/${key}`);
+
+export const createCandidates = (worldcupId: number, newImgInfos: ImgInfo[]): Promise<AxiosResponse> =>
+  axios.post('/api/candidates', { worldcupId, newImgInfos });
