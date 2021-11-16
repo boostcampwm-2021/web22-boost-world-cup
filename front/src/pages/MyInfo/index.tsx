50519ecc975d9f377aaedb61a7278d14b7bb1b8d
119wjw <119wjw@naver.com>
2021-11-16 23:11:31 +0900

    MyInfo page 구현

---
+++ front/src/pages/MyInfo/index.tsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import Header from '../../components/Header';
+import logo from '../../images/logo.png';
+
+const MyInfo = (): JSX.Element => {
+  return (
+    <>
+      <Header type="header" />
+      <Container>
+        <img src={logo} alt="logo" width="220px" height="220px" />
+        <Title>내 정보</Title>
+        <MenuContainer>
+          <Link to="/profile">
+            <Menu>내 정보 수정</Menu>
+          </Link>
+          <Link to="/leave">
+            <Menu>회원 탈퇴</Menu>
+          </Link>
+        </MenuContainer>
+      </Container>
+    </>
+  );
+};
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 628px;
+  height: 811px;
+  box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
+  position: relative;
+  left: 50%;
+  transform: translate(-50%, 5%);
+`;
+
+const Title = styled.div`
+  ${({ theme }) => theme.fontStyle.h1Bold}
+  position : relative;
+  top: -60px;
+`;
+
+const MenuContainer = styled.div`
+  display: flex;
+  height: 200px;
+  flex-direction: column;
+  justify-content: space-between;
+  margin: 140px 0;
+`;
+
+const Menu = styled.div`
+  ${({ theme }) => theme.fontStyle.h3};
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 402px;
+  height: 75px;
+  background-color: ${({ theme }) => theme.color.primary};
+  border-radius: 10px;
+  box-shadow: 0px 4px 0px rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${({ theme }) => theme.color.pink};
+  }
+`;
+
+export default MyInfo;
