d76c9b774ec070217c36fad6e9d8f37f6a2cf956
giljung456 <uhwan0723@gmail.com>
2021-11-16 15:55:44 +0900

    feat: Edit페이지에서 currentTab이 변할 때마다 metadata를 요청하고, 받은 응답으로 상태를 변경하는 로직 추가 #89
    첫 번째 탭에서 제목 변경/설명 변경/이미지 추가를 하는 즉시 db에 반영해줄 것이기에, 두 번째 탭과의 동기화를 위해서 탭을 변경할 때마다 최신의 metadata를 받아와서 반영합니다.

--- front/src/pages/Edit/index.tsx
+++ front/src/pages/Edit/index.tsx
@@ -1,16 +1,44 @@
 import React, { useState, useMemo, useEffect } from 'react';
 import styled from 'styled-components';
 import { Header, MakeWorldcupForm, ImgTable, MakePageTabBar } from '../../components';
+import useApiRequest, { NULL, REQUEST, SUCCESS, FAILURE } from '../../hooks/useApiRequest';
 import { useTabBar, useWorldcupForm, useImgInfos, usePagination } from '../../hooks';
+import { getWorldcupMetadata } from '../../utils/api/worldcups';
 
 function Edit(): JSX.Element {
   const PAGINATION_LIMIT = 8;
+  const [previews, setPreviews] = useState([]);
+  const [totalCnt, setTotalCnt] = useState(0);
   const [worldcupFormState, worldcupFormDispatcher] = useWorldcupForm();
   const [currentTab, onTabChange] = useTabBar();
   const [imgInfos, imgInfosDispatcher] = useImgInfos();
-  const [currentPage, offset, lastPage, onPageChange] = usePagination(imgInfos.length, PAGINATION_LIMIT);
-  const [previews, setPreviews] = useState([]);
+  const [currentPage, offset, lastPage, onPageChange] = usePagination(totalCnt, PAGINATION_LIMIT);
+  const [getMetadataResult, getMetadataDispatcher] = useApiRequest(getWorldcupMetadata);
   const worldcupId = useMemo(() => location.pathname.split('/')[2], [location]);
+
+  useEffect(() => {
+    getMetadataDispatcher({ type: REQUEST, requestProps: [worldcupId] });
+  }, [currentTab, worldcupId]);
+
+  useEffect(() => {
+    const { type } = getMetadataResult;
+    switch (type) {
+      case NULL:
+      case REQUEST:
+        return;
+      case SUCCESS:
+        const { data: metadata } = getMetadataResult;
+        const { totalCnt, title, description } = metadata;
+        worldcupFormDispatcher({ type: 'CHANGE_TITLE', payload: title });
+        worldcupFormDispatcher({ type: 'CHANGE_DESC', payload: description });
+        setTotalCnt(totalCnt);
+        return;
+      case FAILURE:
+        return;
+      default:
+        throw new Error('Unexpected request type');
+    }
+  }, [getMetadataResult]);
 
   return (
     <>
@@ -35,7 +63,7 @@ function Edit(): JSX.Element {
         )}
         {currentTab === 2 && (
           <ImgTable
-            imgInfos={imgInfos.slice(offset, offset + PAGINATION_LIMIT)}
+            imgInfos={imgInfos}
             currentPage={currentPage}
             lastPage={lastPage}
             offset={offset}
