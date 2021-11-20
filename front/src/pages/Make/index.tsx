c49e9198722ab51746109015433a3c60c44e8304
giljung456 <uhwan0723@gmail.com>
2021-11-20 21:43:14 +0900

    refactor: usePagination의 변경사항을 Make에 반영 #118

--- front/src/pages/Make/index.tsx
+++ front/src/pages/Make/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Header, MakeWorldcupForm, ImgTable, TabBar, StoreBtns } from '../../components';
-import { usePagination, useTabBar, useImgInfos, useWorldcupForm } from '../../hooks';
+import { usePaginationSync, useTabBar, useImgInfos, useWorldcupForm } from '../../hooks';
 import { ImgInfo } from '../../types/Datas';
 
 function Make(): JSX.Element {
@@ -10,7 +10,12 @@ function Make(): JSX.Element {
   const PAGINATION_LIMIT = 8;
   const [previewStartIdx, setPreviewStartIdx] = useState(0);
   const [currentTab, onTabChange] = useTabBar(() => setPreviewStartIdx(imgInfos.length));
-  const [currentPage, offset, lastPage, onPageChange] = usePagination(imgInfos.length, PAGINATION_LIMIT);
+  const [pageItems, currentPage, offset, lastPage, onPageChange] = usePaginationSync<ImgInfo>(
+    imgInfos.length,
+    PAGINATION_LIMIT,
+    imgInfos,
+    [currentTab, imgInfos.length],
+  );
   const tabTitle = ['1. 기본정보 수정 / 이미지 업로드', '2. 이미지 이름 수정 / 삭제'];
 
   const getSignedURLsSuccessEffect = (newImgInfos: ImgInfo[]) => {
@@ -41,7 +46,7 @@ function Make(): JSX.Element {
         )}
         {currentTab === 2 && (
           <ImgTable
-            imgInfos={imgInfos.slice(offset, offset + PAGINATION_LIMIT)}
+            imgInfos={pageItems}
             currentPage={currentPage}
             lastPage={lastPage}
             offset={offset}
