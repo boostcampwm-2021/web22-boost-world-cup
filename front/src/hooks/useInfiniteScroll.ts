8baf6a65db50497fc8deb03b17b1a036a7039b11
giljung456 <uhwan0723@gmail.com>
2021-11-22 16:18:59 +0900

    feat: useInfiniteScroll 추가 #122
    Main과 Comment 컴포넌트에서 무한스크롤 관련 로직이 중복되게 쓰였기 때문에, 이를 useInfiniteScroll로 분리하고자 합니다.

---
+++ front/src/hooks/useInfiniteScroll.ts
@@ -0,0 +1,58 @@
+import { useState, useEffect, useRef } from 'react';
+import { AxiosResponse } from 'axios';
+import useApiRequest, { REQUEST } from './useApiRequest';
+
+const THRESHOLD = 0.4;
+
+const useInfiniteScroll = <T>(
+  limit: number,
+  getItems: (offset: number, limit: number, ...args: any[]) => Promise<AxiosResponse>,
+  requestProps: any[],
+) => {
+  const [offset, setOffset] = useState(0);
+  const [isClickMore, setIsClickMore] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [items, setItems] = useState<T[]>([]);
+  const observer = useRef<IntersectionObserver | null>(null);
+  const target = useRef<HTMLDivElement | null>(null);
+  const onGetItemsSuccess = (newItems: T[]) => {
+    setIsLoading(false);
+    if (!newItems.length) {
+      (observer.current as IntersectionObserver).disconnect();
+      return;
+    }
+    setOffset(offset + limit);
+    if (!offset) {
+      setItems([...newItems]);
+      return;
+    }
+    setItems([...items, ...newItems]);
+  };
+  const getItemsDispatcher = useApiRequest(getItems, onGetItemsSuccess);
+
+  const onIntersect = ([entry]: IntersectionObserverEntry[], observer: IntersectionObserver) => {
+    if (!entry.isIntersecting || isLoading) return;
+    setIsLoading(true);
+    getItemsDispatcher({ type: REQUEST, requestProps: [offset, limit, ...requestProps] });
+    observer.unobserve(entry.target);
+  };
+
+  const onClickMoreBtn = () => setIsClickMore(!isClickMore);
+
+  useEffect(() => {
+    if (!isClickMore) return;
+    observer.current = new IntersectionObserver(onIntersect, { threshold: THRESHOLD });
+    observer.current.observe(target.current as HTMLDivElement);
+    return () => (observer.current as IntersectionObserver).disconnect();
+  }, [offset, isClickMore]);
+
+  useEffect(() => {
+    if (offset !== 0) return;
+    setIsLoading(true);
+    getItemsDispatcher({ type: REQUEST, requestProps: [offset, limit, ...requestProps] });
+  }, [offset]);
+
+  return { items, target, isLoading, isClickMore, onClickMoreBtn, setOffset, setItems };
+};
+
+export default useInfiniteScroll;
