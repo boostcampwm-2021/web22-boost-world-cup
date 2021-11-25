1a11eb0167cf1568d755fffc2e003fd5f4a5c695
Yongjin Kim <dydwls0669@gmail.com>
2021-11-25 17:00:51 +0900

    design: 로딩 위치 변경

--- front/src/pages/Loading/index.tsx
+++ front/src/pages/Loading/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
 import Loader from '../../components/Loader';
 
 function Loading(): JSX.Element {
-  return <Loader />;
+  return (
+    <Wrapper>
+      <Loader />;
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  padding-top: 30%;
+`;
 
 export default Loading;
