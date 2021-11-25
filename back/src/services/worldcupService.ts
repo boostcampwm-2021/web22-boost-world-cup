5e65579ecbddce6f85b3e1820a62f25c2f68158d
giljung456 <uhwan0723@gmail.com>
2021-11-25 15:50:00 +0900

    refactor: worldcupService에서 url을 사용하는 로직들을 key를 사용하도록 변경 #156

--- back/src/services/worldcupService.ts
+++ back/src/services/worldcupService.ts
@@ -57,9 +57,7 @@ export const save = async (title: string, description: string, keywordNames: str
     Promise.all(keywordNames.map((name: string) => findOrCreateTag(name))),
     findUserById(userId),
   ]);
-  const [thumbnail1, thumbnail2] = imgInfos
-    .slice(0, 2)
-    .map(({ key }) => `${process.env.IMG_URL_END_POINT}/${key}.webp`);
+  const [thumbnail1, thumbnail2] = imgInfos.slice(0, 2).map(({ key }) => key);
 
   const {
     identifiers: [{ id }],
