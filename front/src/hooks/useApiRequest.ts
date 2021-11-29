53daea39c17871fcad22f7dbb45bc98ff2097d5f
giljung456 <uhwan0723@gmail.com>
2021-11-29 17:18:56 +0900

    chore: useApiRequest에서 action의 type 상수화 했던 것 제거

--- front/src/hooks/useApiRequest.ts
+++ front/src/hooks/useApiRequest.ts
@@ -3,11 +3,6 @@ import { AxiosResponse, AxiosError } from 'axios';
 import { RequestState } from '../types/States';
 import Reducer from '../types/Reducer';
 
-export const NULL = 'NULL';
-export const REQUEST = 'REQUEST';
-export const FAILURE = 'FAILURE';
-export const SUCCESS = 'SUCCESS';
-
 const requestReducer = <T>(state: RequestState<T>, action: RequestState<T>) => action;
 
 const fetchData = async <T>(
@@ -19,12 +14,12 @@ const fetchData = async <T>(
     const {
       data: { data: response },
     } = requestProps ? await apiRequest(...requestProps) : await apiRequest();
-    requestDispatcher({ type: SUCCESS, response });
+    requestDispatcher({ type: 'SUCCESS', response });
   } catch (error) {
     const err = error as AxiosError;
     // eslint-disable-next-line no-console
     console.error(err.response?.data.error);
-    requestDispatcher({ type: FAILURE, statusCode: err.response?.status });
+    requestDispatcher({ type: 'FAILURE', statusCode: err.response?.status });
   }
 };
 
@@ -34,28 +29,28 @@ const useApiRequest = <T>(
   onFailure?: (status: number) => void,
 ): React.Dispatch<RequestState<T>> => {
   const [requestState, requestDispatcher] = useReducer<Reducer<RequestState<T>, RequestState<T>>>(requestReducer, {
-    type: NULL,
+    type: 'NULL',
   });
 
   useEffect(() => {
     switch (requestState.type) {
-      case NULL:
+      case 'NULL':
         return;
-      case REQUEST: {
+      case 'REQUEST': {
         const { requestProps } = requestState;
         fetchData(apiRequest, requestDispatcher, requestProps);
         return;
       }
-      case SUCCESS: {
+      case 'SUCCESS': {
         const { response } = requestState;
         if (onSuccess) onSuccess(response as T);
-        requestDispatcher({ type: NULL });
+        requestDispatcher({ type: 'NULL' });
         return;
       }
-      case FAILURE: {
+      case 'FAILURE': {
         const { statusCode } = requestState;
         if (onFailure && statusCode) onFailure(statusCode);
-        requestDispatcher({ type: NULL });
+        requestDispatcher({ type: 'NULL' });
         return;
       }
       default:
